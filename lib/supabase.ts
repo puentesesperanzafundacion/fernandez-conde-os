3e02da86fa5ccedace4f58759ca3f6a053ab0a76
puentesesperanzafundacion <puentes.esperanza.fundacion@gmail.com>
2026-08-25 09:33:25 -0600

    Add Supabase client for V1.1

---
+++ lib/supabase.ts
@@ -0,0 +1,5 @@
+import { createClient } from "@supabase/supabase-js";
+export const supabase = createClient(
+  "https://yhwxwteqoipwqghwemmo.supabase.co",
+  "sb_publishable_mc5W09t90QR51l3S_JpKcg_V4Zict_0"
+);
